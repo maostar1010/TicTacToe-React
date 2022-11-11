55175c08a6c61ea1d9284ec3525e394671b55203
Mary <maostar1010@gmail.com>
2022-11-11 16:10:10 -0700

    x and o taking turns

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
@@ -24,8 +25,11 @@ class Board extends React.Component {
         // .slice() to create a copy of the squares array 
         // to modify instead of modifying the existing array
         // IMMUTABILITY
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
