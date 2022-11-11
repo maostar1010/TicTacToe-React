0160ebe7e11b1608be964c0cee0c19b54a9f5b65
Mary <maostar1010@gmail.com>
2022-11-11 15:56:13 -0700

    handleCilck added, state saved in Board

--- index.js
+++ index.js
@@ -3,31 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
       return (
         <button 
             className="square" 
-            onClick={()=> this.setState({value: 'X'})}
+            onClick={()=> this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
-    constructur(props) {
+    constructor(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
         };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     renderSquare(i) {
