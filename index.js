ecca2c3094e90e5556bd2aaad97d89f86f4929d5
Mary <maostar1010@gmail.com>
2022-11-11 17:44:55 -0700

    correct squares, labelling the moves

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="sqaure" onClick={props.onClick}>
+        <button className="square" onClick={props.onClick}>
             {props.value}
       </button>
     );
